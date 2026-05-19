a48ce2e311f2715ea5899c63628fc2392979e517
habitualist <nnaochinobinna001@gmail.com>
2026-05-19 13:53:07 +0100

    update Google OAuth callback to production URL

--- backend/config/Passport/index.js
+++ backend/config/Passport/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID:     process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:  'http://localhost:5000/api/auth/google/callback'
+  callbackURL:  'https://skillswap-pumw.onrender.com/api/auth/google/callback'
 }, googleAuth));
 
 passport.serializeUser((user, done) => done(null, user));
